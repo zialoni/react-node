beddb875445ed2c732ae6a4235bc130eb7fa30de
zia loni <ziadikhan@gmail.com>
2020-04-20 17:11:21 +1000

    move private information into dev.js

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://zia:zia123@zia-123-6nro8.mongodb.net/paul?retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("body-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/user");
 
 mongoose
-  .connect(
-    "mongodb+srv://zia:zia123@zia-123-6nro8.mongodb.net/paul?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useCreateIndex: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json({ "hello~": "Hi ~~ " });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
